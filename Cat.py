45b3a8d332f88894664e149ca4912229b9d69fdc
CherryOFF_OLD <63562425+cherrymich@users.noreply.github.com>
2022-12-06 22:43:21 +0300

    Add files via upload

---
+++ Cat.py
@@ -0,0 +1,7 @@
+class Cats:
+    def __init__(self, name, gender, age):
+        self.name = name
+        self.gender = gender
+        self.age = age
+    def  info_Cats(self):
+        print(f'Имя животного: {self.name}, \n пол: {self.gender}, \n возраст животного: {self.age}')
---
+++ Cats_base.py
@@ -0,0 +1,13 @@
+from Cat import Cats
+
+
+Cat1 = Cats('Семен', 'мальчик','2 год(а)')
+Cat2 = Cats('Барсик', 'мальчик','1 год(а)')
+Cat3 = Cats('Василиса', 'девочка','0 год(а)')
+
+
+
+
+Cat1.info_Cats ()
+Cat2.info_Cats ()
+Cat3.info_Cats ()
