3c557026e476bf753f98f453133d58bab1025dde
CherryOFF_OLD <63562425+cherrymich@users.noreply.github.com>
2023-09-20 11:32:42 +0300

    Update calc.py

--- pythonProject24_2_4/app/calc.py
+++ pythonProject24_2_4/app/calc.py
@@ -1,14 +1,9 @@
-
-
 class Calculator:
-    def multiplication(self, x, y):
+    def multiply(self, x, y):
         return x * y
-
     def division(self, x, y):
         return x / y
-
-    def adding(self, x, y):
-        return x + y
-
     def subtraction(self, x, y):
         return x - y
+    def adding(self, x, y):
+        return x + y
