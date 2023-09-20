b1d7441f85ff1ed2d668d772c02d931f77b94b18
CherryOFF_OLD <63562425+cherrymich@users.noreply.github.com>
2023-09-20 11:36:12 +0300

    Update test.py

--- pythonProject24_2_4/tests/test.py
+++ pythonProject24_2_4/tests/test.py
@@ -2,25 +2,39 @@ import pytest
 
 from app.calc import Calculator
 
-class TestingCalc:
+class TestCalc:
     def setup(self):
         self.calc = Calculator
 
-    def test_adding_success(self): # проверка сложения
-        assert self.calc.adding(self, 5, 7) == 12
+    def test_adding_success(self): # позитивный тест на сложение
+        assert  self.calc.adding(self, 1, 1) == 2
 
-    def test_subtraction_success(self): # проверка вычитания
-        assert self.calc.subtraction(self, 8, 4) == 4
+    def test_adding_unsuccess(self): # негативный тест на сложение
+        assert self.calc.adding(self, 1, 1) == 3
 
-    def test_multiplication_success(self): # проверка умножения
-        assert self.calc.multiplication(self, 2, 3) == 6
 
-    def test_division_success(self): # проверка деления
-        assert self.calc.division(self, 6, 3) == 2
+    def test_subtraction_success(self):  # позитивный тест на вычитание
+        assert self.calc.subtraction(self, 9, 7) == 2
 
-    def test_zero_division(self): # проверка деления на нуль, обрабатывается исключение ZeroDivisionError
+
+
+    def test_zero_division(self):  # тест деление на нуль, обрабатывается исключение ZeroDivisionError
         with pytest.raises(ZeroDivisionError):
-            self.calc.division(self, 2, 0)
+            self.calc.division(self, 1, 0)
+
+
+
+    def test_multiply_success(self):  # позитивный тест на умножение
+        assert self.calc.multiply(self, 2, 5) == 10
+
+
+    def test_multiply_unsuccess(self):   # негативный тест на умножение
+         assert self.calc.multiply(self, 3, 5) == 14
+
+    def test_divsion_success(self):  # позитивный тест на деление
+        assert self.calc.division(self, 8, 4) == 2
+
 
     def teardown(self):
-        print('Завершаем тестирование. Закрываем файл проверки. Выполнение метода TearDown')
+        print("Завершаем тестирование. Закрываем файл проверки. Выполнение метода Teardown")
+
